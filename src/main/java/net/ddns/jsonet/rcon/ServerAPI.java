5e7b145b02071867d45a61869d67b607594fabbd
Jason R. Carrete <jasoncarrete5@gmail.com>
2017-06-20 16:54:09 -0400

    Implemented bare bones server api

---
+++ src/main/java/net/ddns/jsonet/rcon/ServerAPI.java
@@ -0,0 +1,71 @@
+package net.ddns.jsonet.rcon;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+public class ServerAPI {
+	private static ServerAPI instance;
+	
+	public static ServerAPI get() {
+		if (instance == null) {
+			return instance = new ServerAPI();
+		} else {
+			return instance;
+		}
+	}
+	
+	private Socket client;
+	
+	private ServerAPI() {
+		client = new Socket();
+	}
+	
+	/**
+	 * Attempts a to connect to the remote rcon server. If the ServerAPI is alreader connected
+	 * to a remote server, invoking this method will close the connection and create a new connection.
+	 * @param hostname address of the remote server.
+	 * @param port port the remote server is listening on.
+	 * @throws IOException if the connection fails for some reason.
+	 */
+	public void connect(String hostname, int port) throws IOException {
+		if (client.isConnected()) {
+			client.close();
+		}
+		if (client.isClosed()) {
+			client = new Socket();
+		}
+		
+		client.connect(new InetSocketAddress(hostname, port));
+	}
+	
+	public void disconnect() throws IOException {
+		client.close();
+	}
+	
+	/**
+	 * Attempts to authenticate with the connected server.
+	 * @param asciiPassword password to use for authentication.
+	 * @return the request id
+	 */
+	public int authenticate(String asciiPassword) {
+		if (client.isClosed() || !client.isConnected()) {
+			throw new IllegalStateException("Tried to authenticate with a server before connecting to it!");
+		}
+		
+		return 0;
+	}
+	
+	/**
+	 * Attempts to send a command to the connected server.
+	 * @param command command to execute on the server.
+	 * @return the request id.
+	 */
+	public int sendCommand(String command) {
+		if (client.isClosed() || !client.isConnected()) {
+			throw new IllegalStateException("Tried to send a command to a server before connecting to it!");
+		}
+		
+		return 0;
+	}
+}
