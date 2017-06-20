942ce785be9840eae98af68423a62ae034ea6105
Jason R. Carrete <jasoncarrete5@gmail.com>
2017-06-20 13:21:13 -0400

    Removed unused import

--- src/main/java/net/ddns/jsonet/rcon/RconClient.java
+++ src/main/java/net/ddns/jsonet/rcon/RconClient.java
@@ -1,7 +1,6 @@
 package net.ddns.jsonet.rcon;
 
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.Scanner;
 import java.util.logging.FileHandler;
 import java.util.logging.Logger;
